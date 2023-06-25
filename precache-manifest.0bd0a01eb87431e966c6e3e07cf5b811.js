b47db9091209386a53065bc4ffcf1acfa2093585
zaper1402 <ashirkul@gmail.com>
2023-06-25 17:34:30 +0530

    Updates

---
+++ precache-manifest.0bd0a01eb87431e966c6e3e07cf5b811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a7e9092a59e1e792bc5cdf247c0e01",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "8cda43f1cf4eb70a30b2",
+    "url": "/dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "0761b4050261bc04acc7",
+    "url": "/dev-portfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "8cda43f1cf4eb70a30b2",
+    "url": "/dev-portfolio/static/js/2.3a2e0d3c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/dev-portfolio/static/js/2.3a2e0d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0761b4050261bc04acc7",
+    "url": "/dev-portfolio/static/js/main.c4e27fc3.chunk.js"
+  },
+  {
+    "revision": "cfdb1831a76d49a6fafa",
+    "url": "/dev-portfolio/static/js/runtime-main.b4d570c5.js"
+  }
+]);
