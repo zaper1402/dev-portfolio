4c05c962d88f4649c219c3d975409c2f05c71138
zaper1402 <ashirkul@gmail.com>
2023-06-25 22:44:25 +0530

    Updates

--- precache-manifest.90e598cdb240b0323643f5b2bcd5cac3.js
+++ precache-manifest.90e598cdb240b0323643f5b2bcd5cac3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2d73452ea92819cd622e57daed0e7e33",
+    "revision": "11ac331093baffafb2031d31811c3886",
     "url": "/dev-portfolio/index.html"
   },
   {
-    "revision": "bdba5b731331601136e8",
+    "revision": "745028b311d49544019f",
     "url": "/dev-portfolio/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "b0095f525f0c2c64debf",
+    "revision": "4413a6587f5233e3531a",
     "url": "/dev-portfolio/static/css/main.db76a3b0.chunk.css"
   },
   {
-    "revision": "bdba5b731331601136e8",
-    "url": "/dev-portfolio/static/js/2.cc76eaf5.chunk.js"
+    "revision": "745028b311d49544019f",
+    "url": "/dev-portfolio/static/js/2.9da83ca2.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "/dev-portfolio/static/js/2.cc76eaf5.chunk.js.LICENSE.txt"
+    "url": "/dev-portfolio/static/js/2.9da83ca2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b0095f525f0c2c64debf",
-    "url": "/dev-portfolio/static/js/main.36d74705.chunk.js"
+    "revision": "4413a6587f5233e3531a",
+    "url": "/dev-portfolio/static/js/main.d52c24c8.chunk.js"
   },
   {
     "revision": "1e10e024f0899dac1144",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev-portfolio/precache-manifest.16129e5074d8cd322d74aaa80510d1a0.js"
+  "/dev-portfolio/precache-manifest.90e598cdb240b0323643f5b2bcd5cac3.js"
 );
 
 self.addEventListener('message', (event) => {
